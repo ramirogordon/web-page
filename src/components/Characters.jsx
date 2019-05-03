32fbc637f6fbdd99afafb379c39a01a08e2c1fb3
Ramiro Gordon <ramiro.gordon@globant.com>
2019-05-03 16:59:38 -0300

    cards css very nice :)

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -9,82 +9,174 @@ const Characters = () => {
       <Navbar />
       <div className="full-characters">
         <h1>Character Sets</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Eligendi, ducimus dolores id sed sunt consectetur nesciunt 
-          vitae laboriosam ipsa repudiandae, repellat labore temporibus 
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Eligendi, ducimus dolores id sed sunt consectetur nesciunt
+          vitae laboriosam ipsa repudiandae, repellat labore temporibus
           suscipit dolore libero minima. Vero, debitis non.
         </p>
         <div className="characters-content">
           <div className="character-card">
             <div className="character-image">
-              <img src="http://pngriver.com/wp-content/uploads/2018/04/Download-Archer-PNG-Transparent.png" alt="img1"/>
+              <img src="http://pngriver.com/wp-content/uploads/2018/04/Download-Archer-PNG-Transparent.png" alt="img1" />
             </div>
             <div className="character-data">
-              <h2>This is a character card</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing 
-                elit. Veritatis, voluptas!
-              </p>
+              <div className="character-description">
+                <p>level 7</p>
+                <h2>Volomir</h2>
+                <div className="description-value">
+                  Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit. Veritatis, voluptas!
+                </div>
+              </div>
+              <div className="character-stats">
+                <div className="stats">
+                  <div className="stats-value">
+                    185
+                  </div>
+                  <div className="stats-name">
+                    Life
+                  </div>
+                </div>
+                <div className="stats">
+                  <div className="stats-value">
+                    90
+                  </div>
+                  <div className="stats-name">
+                    Magic Armor
+                  </div>
+                </div>
+                <div className="stats no-border">
+                  <div className="stats-value">
+                    90
+                  </div>
+                  <div className="stats-name">
+                    Physical Armor
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div className="character-card">
             <div className="character-image">
-              <img src="http://pngriver.com/wp-content/uploads/2018/04/Download-Archer-PNG-Transparent.png" alt="img1"/>
+              <img src="http://pngriver.com/wp-content/uploads/2018/04/Download-Archer-PNG-Transparent.png" alt="img1" />
             </div>
             <div className="character-data">
-              <h2>This is a character card</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing 
-                elit. Veritatis, voluptas!
-              </p>
+              <div className="character-description">
+                <p>level 7</p>
+                <h2>Volomir</h2>
+                <div className="description-value">
+                  Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit. Veritatis, voluptas!
+                </div>
+              </div>
+              <div className="character-stats">
+                <div className="stats">
+                  <div className="stats-value">
+                    185
+                  </div>
+                  <div className="stats-name">
+                    Life
+                  </div>
+                </div>
+                <div className="stats">
+                  <div className="stats-value">
+                    90
+                  </div>
+                  <div className="stats-name">
+                    Magic Armor
+                  </div>
+                </div>
+                <div className="stats no-border">
+                  <div className="stats-value">
+                    90
+                  </div>
+                  <div className="stats-name">
+                    Physical Armor
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div className="character-card">
             <div className="character-image">
-              <img src="http://pngriver.com/wp-content/uploads/2018/04/Download-Archer-PNG-Transparent.png" alt="img1"/>
+              <img src="http://pngriver.com/wp-content/uploads/2018/04/Download-Archer-PNG-Transparent.png" alt="img1" />
             </div>
             <div className="character-data">
-              <h2>This is a character card</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing 
-                elit. Veritatis, voluptas!
-              </p>
+              <div className="character-description">
+                <p>level 7</p>
+                <h2>Volomir</h2>
+                <div className="description-value">
+                  Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit. Veritatis, voluptas!
+                </div>
+              </div>
+              <div className="character-stats">
+                <div className="stats">
+                  <div className="stats-value">
+                    185
+                  </div>
+                  <div className="stats-name">
+                    Life
+                  </div>
+                </div>
+                <div className="stats">
+                  <div className="stats-value">
+                    90
+                  </div>
+                  <div className="stats-name">
+                    Magic Armor
+                  </div>
+                </div>
+                <div className="stats no-border">
+                  <div className="stats-value">
+                    90
+                  </div>
+                  <div className="stats-name">
+                    Physical Armor
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div className="character-card">
             <div className="character-image">
-              <img src="http://pngriver.com/wp-content/uploads/2018/04/Download-Archer-PNG-Transparent.png" alt="img1"/>
+              <img src="http://pngriver.com/wp-content/uploads/2018/04/Download-Archer-PNG-Transparent.png" alt="img1" />
             </div>
             <div className="character-data">
-              <h2>This is a character card</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing 
-                elit. Veritatis, voluptas!
-              </p>
-            </div>
-          </div>
-          <div className="character-card">
-            <div className="character-image">
-              <img src="http://pngriver.com/wp-content/uploads/2018/04/Download-Archer-PNG-Transparent.png" alt="img1"/>
-            </div>
-            <div className="character-data">
-              <h2>This is a character card</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing 
-                elit. Veritatis, voluptas!
-              </p>
-            </div>
-          </div>
-          <div className="character-card">
-            <div className="character-image">
-              <img src="http://pngriver.com/wp-content/uploads/2018/04/Download-Archer-PNG-Transparent.png" alt="img1"/>
-            </div>
-            <div className="character-data">
-              <h2>This is a character card</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing 
-                elit. Veritatis, voluptas!
-              </p>
+              <div className="character-description">
+                <p>level 7</p>
+                <h2>Volomir</h2>
+                <div className="description-value">
+                  Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit. Veritatis, voluptas!
+                </div>
+              </div>
+              <div className="character-stats">
+                <div className="stats">
+                  <div className="stats-value">
+                    185
+                  </div>
+                  <div className="stats-name">
+                    Life
+                  </div>
+                </div>
+                <div className="stats">
+                  <div className="stats-value">
+                    90
+                  </div>
+                  <div className="stats-name">
+                    Magic Armor
+                  </div>
+                </div>
+                <div className="stats no-border">
+                  <div className="stats-value">
+                    90
+                  </div>
+                  <div className="stats-name">
+                    Physical Armor
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
