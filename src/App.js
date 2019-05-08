04f0bbbabf1ed1de65728ecacc4616f764a5bcab
Ramiro Gordon <ramiro.gordon@globant.com>
2019-05-08 15:27:43 -0300

    not important

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Characters from './components/Characters';
+import CustomCharacter from './components/CustomCharacter';
 import Equipment from './components/Equipment';
 import Abilities from './components/Abilities';
 import store from './redux/Store';
@@ -14,7 +15,8 @@ function App() {
       <Router>
         <div>
           <Route exact path="/" component={Home}/>
-          <Route path="/characters" component={Characters}/>
+          <Route exact path="/characters" component={Characters}/>
+          <Route path="/characters/custom" component={CustomCharacter} />
           <Route path="/equipment" component={Equipment}/>
           <Route path="/abilities" component={Abilities}/>
         </div>
--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Character = (props) => {
     console.log(props.characters);
@@ -6,44 +7,46 @@ const Character = (props) => {
     const listCharacters = characters.map((character, i) => {
         return (
             <div className="character-card" key={i}>
-                <div className="character-image">
-                    <img src={character.img} alt="img1" />
-                </div>
-                <div className="character-data">
-                    <div className="character-description">
-                        <p>Level {character.level}</p>
-                        <h2>{character.name}</h2>
-                        <div className="description-value">
-                            {character.description}
+                <Link to="/characters/custom">
+                    <div className="character-image">
+                        <img src={character.img} alt="img1" />
+                    </div>
+                    <div className="character-data">
+                        <div className="character-description">
+                            <p>Level {character.level}</p>
+                            <h2>{character.name}</h2>
+                            <div className="description-value">
+                                {character.description}
+                            </div>
+                        </div>
+                        <div className="character-stats">
+                            <div className="stats">
+                                <div className="stats-value">
+                                    {character.life}
+                                </div>
+                                <div className="stats-name">
+                                    Life
+                                </div>
+                            </div>
+                            <div className="stats">
+                                <div className="stats-value">
+                                    {character.magicArmor}
+                                </div>
+                                <div className="stats-name">
+                                    Magic Armor
+                                </div>
+                            </div>
+                            <div className="stats no-border">
+                                <div className="stats-value">
+                                    {character.physicalArmor}
+                                </div>
+                                <div className="stats-name">
+                                    Physical Armor
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="character-stats">
-                        <div className="stats">
-                            <div className="stats-value">
-                                {character.life}
-                            </div>
-                            <div className="stats-name">
-                                Life
-                            </div>
-                        </div>
-                        <div className="stats">
-                            <div className="stats-value">
-                                {character.magicArmor}
-                            </div>
-                            <div className="stats-name">
-                                Magic Armor
-                            </div>
-                        </div>
-                        <div className="stats no-border">
-                            <div className="stats-value">
-                                {character.physicalArmor}
-                            </div>
-                            <div className="stats-name">
-                                Physical Armor
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                </Link>
             </div>
         )
     })
